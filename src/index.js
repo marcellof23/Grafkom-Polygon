fbef487da4a5891ff5eb1bf437b80a5e72df261b
Marcello Faria <01faria.marcello@gmail.com>
2022-02-22 23:29:25 +0700

    refactor function and add styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 import { initShaders } from "./shaders/init";
 import { VSHADER_SOURCE, FSHADER_SOURCE, offsetLoc } from "./shaders/const";
 import { click } from "./event/click";
+import { initVertexBuffers } from "./shaders/vertex";
 
 function main() {
   // Retrieve  canvas element
@@ -39,38 +40,14 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-//Make the BO for making triangle
-function initVertexBuffers(gl) {
-  var vertices = new Float32Array([0.0, 0.1, -0.1, -0.1, 0.1, -0.1]);
-  var n = 3;
-
-  //Create a buffer Object
-  var positionBuffer = gl.createBuffer();
-  if (!positionBuffer) {
-    console.log("Failed to create the buffer object");
-    return -1;
-  }
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-  var a_Position = gl.getAttribLocation(gl.program, "a_Position");
-  if (a_Position < 0) {
-    console.log("Failed to get the storage location  of a_Position");
-    return -1;
-  }
-
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  return n;
-}
-
 function canvas() {
   const element = document.createElement("div");
-  element.innerHTML = `<canvas id="webgl"></canvas>`;
+  element.className = "canvas-wrapper";
+  element.innerHTML = `<canvas id="webgl" width="600" height="600"></canvas>`;
+
   return element;
 }
+
 window.onload = main;
 
 document.body.appendChild(canvas());
